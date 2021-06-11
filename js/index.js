928a0044830979fb8cbf646c738b6d88a9deedcc
Lex <ckfly4014737@gmail.com>
2021-06-11 19:39:24 +0800

    6/11 開頭動畫界面設定

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#button_gotoform_page_opening").click(function(){
+        var msg = '開關切換：' + $("#sound_switch opion:chechked").val();
+        alert(msg);
+    })
+});
